ccc462654345c1cfb6f321894d046506cf9f2154
ArnabDutta246 <arnab2461997@gmail.com>
2021-12-17 22:02:45 +0530

    git push

--- src/app/pages/themes/themes.page.ts
+++ src/app/pages/themes/themes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ThemeService } from 'src/app/shared/theme/theme.service';
 
 @Component({
   selector: 'app-themes',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThemesPage implements OnInit {
   themeColor:string;
-  constructor() { }
+  constructor(private themeServ:ThemeService) { }
 
   ngOnInit() {
   }
@@ -15,6 +16,7 @@ export class ThemesPage implements OnInit {
   // change select theme
   changeSelectItem(e){
     console.log("selected theme color",this.themeColor);
+    this.themeServ.activeTheme(this.themeColor);
   }
 
 }
